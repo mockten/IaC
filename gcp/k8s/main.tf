8737d10ad82e03d5d520c94ecdaf7d79893e5fe2
ryo29wx <59174187+ryo29wx@users.noreply.github.com>
2023-06-20 20:23:24 +0900

    fix/ to enable public endpoint on master node

--- gcp/k8s/main.tf
+++ gcp/k8s/main.tf
@@ -18,15 +18,19 @@ resource "google_container_cluster" "mockten_k8s_cluster" {
 
   private_cluster_config {
     enable_private_nodes = true
-    enable_private_endpoint = true
+    enable_private_endpoint = false
     master_ipv4_cidr_block = var.k8s_master_cidr
 
     master_global_access_config {
-    enabled = false
+      enabled = false
     }
   }
 
   master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = "192.0.2.127/32"
+      display_name = "my-laptop-nw"
+    }
   }
 
   maintenance_policy {
