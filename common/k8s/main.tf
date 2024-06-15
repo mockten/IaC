453ee9eea5940793c82e7f94a5585558a94490a6
YabamuroSan <32224266+yabamuro@users.noreply.github.com>
2024-06-16 03:17:44 +0800

    Update main.tf

--- common/k8s/main.tf
+++ common/k8s/main.tf
@@ -13,52 +13,13 @@ resource "kubernetes_secret" "ghcr" {
     })
   }
 }
-resource "kubernetes_deployment" "ecfront" {
-  metadata {
-    name = "ecfront-deploy"
-    namespace = "default"
-  }
-  spec {
-    replicas = 2
-    selector {
-      match_labels = {
-        app = "ecfront"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = "ecfront"
-        }
-      }
-      spec {
-        image_pull_secrets {
-          name = kubernetes_secret.ghcr.metadata[0].name
-        }
-        container {
-          name  = "ecfront"
-          image = "ghcr.io/mockten/mockten/ecfront:latest"
-          port {
-            container_port = 80
-          }
-        }
-      }
-    }
-  }
+
+module "ecfront" {
+  source = "./ecfront"
+  secret_name = kubernetes_secret.ghcr.metadata[0].name
 }
-resource "kubernetes_service" "ecfront" {
-  metadata {
-    name      = "ecfront-service"
-    namespace = "default"
-  }
-  spec {
-    selector = {
-      app = "ecfront"
-    }
-    port {
-      name        = "http"
-      port        = 80
-      target_port = 80
-    }
-  }
+
+module "uam" {
+  source = "./uam"
+  secret_name = kubernetes_secret.ghcr.metadata[0].name
 }
