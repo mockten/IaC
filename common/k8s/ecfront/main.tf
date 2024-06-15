164feb355edb763052fac6b4314648636c4c9bf9
YabamuroSan <32224266+yabamuro@users.noreply.github.com>
2024-06-16 03:17:37 +0800

    Create main.tf

---
+++ common/k8s/ecfront/main.tf
@@ -0,0 +1,49 @@
+resource "kubernetes_deployment" "ecfront" {
+  metadata {
+    name = "ecfront-deploy"
+    namespace = "default"
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "ecfront"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "ecfront"
+        }
+      }
+      spec {
+        image_pull_secrets {
+          name = var.secret_name
+        }
+        container {
+          name  = "ecfront"
+          image = "ghcr.io/mockten/mockten/ecfront:latest"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+resource "kubernetes_service" "ecfront" {
+  metadata {
+    name      = "ecfront-service"
+    namespace = "default"
+  }
+  spec {
+    selector = {
+      app = "ecfront"
+    }
+    port {
+      name        = "http"
+      port        = 80
+      target_port = 80
+    }
+  }
+}
