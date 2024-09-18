74c1584a72f4ce367f2969b504e30f702565bc77
YabamuroSan <32224266+yabamuro@users.noreply.github.com>
2024-09-18 16:06:21 +0800

    Update main.tf

--- azure/nw/main.tf
+++ azure/nw/main.tf
@@ -34,5 +34,5 @@ resource "azurerm_subnet" "mockten_pri_subnet2" {
 
 resource "azurerm_subnet_network_security_group_association" "app-subnet-nsg-association" {
   subnet_id                 = azurerm_subnet.mockten_pri_subnet1.id
-  network_security_group_id = azurerm_network_security_group.mockten_app_nsg.id
+  network_security_group_id = var.nsg_id
 }
