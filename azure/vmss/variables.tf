d3fb99c15c21ec38d168502b26dffa1075b73ee0
YabamuroSan <32224266+yabamuro@users.noreply.github.com>
2024-09-18 16:25:00 +0800

    Create variables.tf

---
+++ azure/vmss/variables.tf
@@ -0,0 +1,12 @@
+variable "resource_group_name" {
+    type        = string
+    default     = null
+}
+variable "location" {
+    type        = string
+    default     = null
+}
+variable "mockten_pri_subnet1" {
+    type        = string
+    default     = null
+}
