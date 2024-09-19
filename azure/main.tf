3983c4c9eb0ea714c23a900eb20f9f37f8391527
YabamuroSan <32224266+yabamuro@users.noreply.github.com>
2024-09-19 13:52:49 +0800

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -51,6 +51,17 @@ module "vmss" {
   resource_group_name = var.resource_group_name
   location            = var.location
   mockten_pri_subnet1 = module.nw.mockten_pri_subnet1
+  vmss_sku            = var.vmss_sku
+  vmss_tier           = var.vmss_tier
+  vmss_capacity       = var.vmss_capacity
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  data_disk_size_gb   = var.data_disk_size_gb
+  managed_disk_type   = var.managed_disk_type
+  os_image_publisher  = var.os_image_publisher
+  os_image_offer      = var.os_image_offer
+  os_image_sku        = var.os_image_sku
+  os_image_version    = var.os_image_version
 
   providers = {
     azurerm = azurerm.azure
