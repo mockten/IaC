8acc939bad55762f36c86e058c7c78989628fa3f
YabamuroSan <32224266+yabamuro@users.noreply.github.com>
2024-10-07 15:44:33 +0800

    Update main.tf

--- azure/vmss/main.tf
+++ azure/vmss/main.tf
@@ -1,35 +1,49 @@
-resource "azurerm_kubernetes_cluster" "mockten_aks" {
-  name                = "mockten-aks"
-  resource_group_name = var.resource_group_name
+resource "azurerm_virtual_machine_scale_set" "mockten_vmss" {
+  name                = "mockten-vmss"
   location            = var.location
-  dns_prefix          = "mockten"
+  resource_group_name = var.resource_group_name
+  sku {
+    name     = var.vmss_sku
+    tier     = var.vmss_tier
+    capacity = var.vmss_capacity
+  }
+  
+  upgrade_policy_mode = "Manual"
 
-  default_node_pool {
-    name                = "mocktenpool"
-    node_count          = var.vmss_capacity  
-    vm_size             = var.vmss_sku       
-    vnet_subnet_id      = var.mockten_pri_subnet1
-    os_disk_size_gb     = var.data_disk_size_gb
-    enable_auto_scaling = false
+  network_profile {
+    name    = "mockten-network-profile"
+    primary = true
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = var.mockten_pri_subnet1
+    }
   }
 
-  identity {
-    type = "SystemAssigned"
+  os_profile {
+    computer_name_prefix = "vmss"
+    admin_username       = var.admin_username
+    admin_password       = var.admin_password
   }
 
-  tags = {
-    Environment = "development" 
+  storage_profile_os_disk {
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = var.managed_disk_type
+  }
+
+  storage_profile_data_disk {
+    lun               = 0
+    caching           = "ReadWrite"
+    create_option     = "Empty"
+    disk_size_gb      = var.data_disk_size_gb
+    managed_disk_type = var.managed_disk_type
+  }
+
+  storage_profile_image_reference {
+    publisher = var.os_image_publisher
+    offer     = var.os_image_offer
+    sku       = var.os_image_sku
+    version   = var.os_image_version
   }
 }
-
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.mockten_aks.kube_config[0].client_certificate
-  sensitive = true
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.mockten_aks.kube_config_raw
-
-  sensitive = true
-}
-
