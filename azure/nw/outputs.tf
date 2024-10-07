c63058046592a367723a7d03f02d20eb2bdb27db
YabamuroSan <32224266+yabamuro@users.noreply.github.com>
2024-10-07 21:01:25 +0800

    Update outputs.tf

--- azure/nw/outputs.tf
+++ azure/nw/outputs.tf
@@ -2,3 +2,7 @@ output "mockten_pri_subnet1" {
   value = azurerm_subnet.mockten_pri_subnet1.id
   description = "App Subnet ID"
 }
+output "mockten_bastion_subnet" {
+  value = azurerm_subnet.mockten_bastion_subnet.id
+  description = "Bastion Subnet ID"
+}
