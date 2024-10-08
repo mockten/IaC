e1d660ebdd637f52f355ec42320438e561a525f0
YabamuroSan <32224266+yabamuro@users.noreply.github.com>
2024-10-08 09:46:40 +0800

    Update main.tf

--- azure/vmss/main.tf
+++ azure/vmss/main.tf
@@ -53,7 +53,7 @@ resource "azurerm_public_ip" "bastion_ip" {
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
-  sku                 = "Basic"
+  sku                 = "Standard"
 }
 resource "azurerm_bastion_host" "mockten_bastion" {
   name                    = "mockten-bastion"
