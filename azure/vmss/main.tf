775deedd06e0fdc8ad11ed0526a6ccd0a0f024e4
YabamuroSan <32224266+yabamuro@users.noreply.github.com>
2024-10-23 15:38:52 +0800

    Update main.tf

--- azure/vmss/main.tf
+++ azure/vmss/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_virtual_machine_scale_set" "mockten_vmss" {
     custom_data          = base64encode(<<EOT
 #!/bin/bash
 curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
-echo "export GITHUB_PAT=${MOCKTEN_REPO_PAT}" >> /etc/environment
+echo "export GITHUB_PAT=${var.repo_pat}" >> /etc/environment
 EOT
     )
   }
